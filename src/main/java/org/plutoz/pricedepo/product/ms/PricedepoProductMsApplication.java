7897235fbcc28fd60ad0e047d4121cedf4252635
László Csatlós <plutoz01@gmail.com>
2017-01-14 14:18:33 +0100

    Project init

---
+++ src/main/java/org/plutoz/pricedepo/product/ms/PricedepoProductMsApplication.java
@@ -0,0 +1,12 @@
+package org.plutoz.pricedepo.product.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricedepoProductMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricedepoProductMsApplication.class, args);
+	}
+}
---
+++ src/test/java/org/plutoz/pricedepo/product/ms/PricedepoProductMsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.plutoz.pricedepo.product.ms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PricedepoProductMsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
