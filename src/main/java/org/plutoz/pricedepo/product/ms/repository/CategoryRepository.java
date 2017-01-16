e8362f4a21edf6e55462cc448203642d61f67790
László Csatlós <plutoz01@gmail.com>
2017-01-16 20:33:08 +0100

    Fix missing @Param annotations, extend @Repository to rest specific

--- src/main/java/org/plutoz/pricedepo/product/ms/repository/CategoryRepository.java
+++ src/main/java/org/plutoz/pricedepo/product/ms/repository/CategoryRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 	
 	public Page<Category> findByNameContainingIgnoreCase(@Param("name") String name, Pageable pageable);
--- src/main/java/org/plutoz/pricedepo/product/ms/repository/ProductRepository.java
+++ src/main/java/org/plutoz/pricedepo/product/ms/repository/ProductRepository.java
@@ -4,13 +4,13 @@ import org.plutoz.pricedepo.product.ms.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 	
-	public Page<Product> findByManufacturer(String manufacturer, Pageable pageable);
+	public Page<Product> findByManufacturer(@Param("manufacturer") String manufacturer, Pageable pageable);
 	
-	public Page<Product> findByBrand(String brand, Pageable pageable);
+	public Page<Product> findByBrand(@Param("brand") String brand, Pageable pageable);
 }
