bcf6a8cd477b0ef976734b78d50847fa6a308ff4
László Csatlós <plutoz01@gmail.com>
2017-01-16 18:17:07 +0100

    Extend Context test with Beans from various application levels

--- src/test/java/org/plutoz/pricedepo/product/ms/ContextTest.java
+++ src/test/java/org/plutoz/pricedepo/product/ms/ContextTest.java
@@ -2,6 +2,10 @@ package org.plutoz.pricedepo.product.ms;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.plutoz.pricedepo.product.ms.repository.CategoryRepository;
+import org.plutoz.pricedepo.product.ms.rest.controller.CategoryController;
+import org.plutoz.pricedepo.product.ms.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +14,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ContextTest {
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
+	@Autowired
+	CategoryService categoryService;
+	
+	@Autowired 
+	CategoryController categoryController;
 
 	@Test
 	public void contextLoads() {
